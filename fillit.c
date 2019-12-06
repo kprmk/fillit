0e391204ba46ad86f8e7e4cc08f21bb1fc1d2f5b
Maurita Brogg <mbrogg@ga-r3.21-school.ru>
2019-12-06 21:58:35 +0300

    START

--- fillit.c
+++ fillit.c
@@ -6,7 +6,7 @@
 /*   By: mbrogg <mbrogg@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/19 21:16:59 by mbrogg            #+#    #+#             */
-/*   Updated: 2019/11/19 21:46:10 by mbrogg           ###   ########.fr       */
+/*   Updated: 2019/12/06 21:54:50 by mbrogg           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,37 +15,46 @@
 #include <stdio.h>
 
 /*
-**	Print table within its width and height that is represents by size
+**	Print table within its width and height that is represented by size
 */
-static	void	print_table(int **table, int size)
+
+void	print_table(int **table, int size)
 {
 	int		i;
 	int		j;
 
 	i = -1;
-	while(++i < size)
+	while (++i < size)
 	{
 		j = -1;
-		while(++j < size)
+		while (++j < size)
 			ft_putchar(table[i][j] + '0');
 		ft_putchar('\n');
 	}
 }
 
-static	int		**init_table(int **table, int size)
+/*
+**	LEAKS!!
+*/
+
+int		**init_table(int **table, int size)
 {
 	int		c;
 
 	c = 0;
 	if (!(table = (int **)malloc(sizeof(int *) * (size + 1))))
 		return (NULL);
-	while(c < size)
+	while (c < size)
 		*(table + c++) = (int *)malloc(sizeof(int) * size);
 	*(table + c) = NULL;
 	return (table);
 }
 
-static int	check_spot(int	**table, int size, int i, int j)
+/*
+**	Amount values
+*/
+
+int	check_spot(int **table, int size, int i, int j)
 {
 	int		res;
 
@@ -61,7 +70,7 @@ static int	check_spot(int	**table, int size, int i, int j)
 	return (res - 1);
 }
 
-static	int		check_tetriminos(int ***table, int size)
+int		check_tetraminos(int ***table, int size)
 {
 	int		sum;
 	int		i;
@@ -69,42 +78,46 @@ static	int		check_tetriminos(int ***table, int size)
 
 	i = -1;
 	sum = 0;
-	while(++i < size)
+	while (++i < size)
 	{
 		j = -1;
-		while(++j < size)
+		while (++j < size)
 		{
 			if (*(*(*table + i) + j))
 			{
 				*(*(*table + i) + j) += check_spot(*table, size, i , j);
-				sum = *(*(*table + i) + j);
+				sum += *(*(*table + i) + j);
 			}
 		}
 	}
-	printf("~%d\n", sum);
-	// print_table(*table, size);
+	// printf("~%d\n", sum);
+	printf("~~~~~~\n");
+	print_table(*table, size);
 	return (sum == 6 ? 1 : 0);
 }
 
 /*
-** LEAKS IBIT_TABLE
+** LEAKS INIT_TABLE
 */
-static	int		check_input_with_table(int	***table, char *str, int *i, int *size)
+
+int		check_input_with_table(int	***table, char *str, int *i, int *size)
 {
-	int 	j;
+	int	j;
 
 	j = 0;
-	if (!(*size) && !(*i))
+	if (!(*i))
+	{
 		*size = ft_strlen(str);
-	if (!(*table = init_table(*table, *size)))
-		return (0);
+		*table = init_table(*table, *size);
+	}
 	if (*str == '\0')
 	{
-		check_tetriminos(table, *size);
+		if (!check_tetraminos(table, *size))
+			return (0);
 		*i = -1;
 	}
-	while(*str)
-		if(*str++ == '#')
+	while (*str)
+		if (*str++ == '#')
 			*(*(*table + *i) + j++) = 1;
 		else
 			*(*(*table + *i) + j++) = 0;
@@ -115,6 +128,7 @@ static	int		check_input_with_table(int	***table, char *str, int *i, int *size)
 **	Each character must be either a block character(’#’)
 **	or an empty character (’.’)
 */
+
 int		check_input_data(char *file_name)
 {
 	char 	*str;
@@ -130,8 +144,6 @@ int		check_input_data(char *file_name)
 		return (-1);
 	while (get_next_line(fd, &str))
 	{
-		// ft_putstr_fd(str, fd_w);
-		// ft_putchar_fd('\n', fd_w);
 		if (!check_input_with_table(&table, str, &i, &size))
 			return (0);
 		i++;
@@ -143,12 +155,11 @@ int		check_input_data(char *file_name)
 	return (1);
 }
 
-int main()
+int main(void)
 {
 	if (!check_input_data("input.txt"))
 		ft_putstr("ERROR\n");
 	else
 		ft_putstr("SUCCESS\n");
-	
 	return (0);
 }
